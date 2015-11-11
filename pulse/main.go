6df05af7e297de154abfa1722db70e44227aeac6
Evan Byrne <evantbyrne@gmail.com>
2015-11-11 14:05:02 -0700

    Email notifications

--- pulse/main.go
+++ pulse/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/smtp"
 	"os"
 
 	"github.com/evantbyrne/pulse"
@@ -9,14 +12,57 @@ import (
 )
 
 var (
-	app = kingpin.New("pulse", "Check the status of a web page.")
-	url = app.Arg("url", "URL to check.").Required().String()
+	app        = kingpin.New("pulse", "Check the status of a web page.")
+	url        = app.Arg("url", "URL to check.").Required().String()
+	configFile = app.Flag("config", "SMTP configuration file.").Short('c').String()
 )
 
+type smtpConfig struct {
+	From         string
+	SmtpHost     string
+	SmtpPassword string
+	SmtpPort     int
+	SmtpUsername string
+	To           string
+}
+
 func main() {
+	var config smtpConfig
+
 	kingpin.MustParse(app.Parse(os.Args[1:]))
+
+	if *configFile != "" {
+		data, configErr := ioutil.ReadFile(*configFile)
+		if configErr != nil {
+			fmt.Printf("Could not read file '%s'\n", *configFile)
+			os.Exit(1)
+		}
+		if configErr = json.Unmarshal(data, &config); configErr != nil {
+			fmt.Println(configErr)
+			os.Exit(1)
+		}
+	}
+
 	if err := pulse.Check(*url); err != nil {
 		fmt.Println(err)
+		if *configFile != "" {
+			auth := smtp.PlainAuth(
+				"",
+				config.SmtpUsername,
+				config.SmtpPassword,
+				config.SmtpHost,
+			)
+			smtpErr := smtp.SendMail(
+				fmt.Sprintf("%s:%d", config.SmtpHost, config.SmtpPort),
+				auth,
+				config.From,
+				[]string{config.To},
+				[]byte(fmt.Sprintf("From: %s\r\nTo: %s\r\nSubject: Pulse Failure @ %s\r\n\r\nError: %s\r\n", config.From, config.To, *url, err)),
+			)
+			if smtpErr != nil {
+				fmt.Println(smtpErr)
+			}
+		}
 		os.Exit(1)
 	}
 }
